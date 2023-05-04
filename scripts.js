2d7a0197f9b69c5882b4acbdcd40138c0c6e1a66
EvyGithub <110493699+EvyGithub@users.noreply.github.com>
2023-05-03 17:33:34 -0700

    some files

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function hi() {
+	alert("Hello!") // Hello!
+}
+
+function schoolSucks() {
+	alert("Well,");
+    alert("yes.");
+    alert("That is true.");
+    alert("People who say school DOESN'T suck,");
+    alert("SHUT UP")
+}
+
+function schoolIsCool() {
+	alert("Well, if you say so,");
+    alert("then you should get out of this website.");
+    alert("Have you read the intro paragraph?");
+    alert("GET OUT.");
+    
+    open("https://www.youtube.com/watch?v=xvFZjo5PgG0") // Rickroll them baby
+}
